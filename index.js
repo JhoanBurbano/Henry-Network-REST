74094a6ede480eac87e29b3549ca0562597f01cc
JhoanBurbano <jhoanburbano@unicauca.edu.co>
2022-02-18 11:46:43 -0500

    s

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ server.use(express.json({ limit: '50mb' }));
 
 server.use(morgan('dev'));
 server.use(cors());
-Server.use((req, res, next) => {
+server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
